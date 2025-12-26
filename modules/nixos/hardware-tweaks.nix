fd6fb300de6ef05b5baf38021df0096effcebea5
yassin <yassin@nixos>
2025-12-26 12:09:18 +0100

    feat: enable zram swap with zstd compression and 50% memory allocation

--- modules/nixos/hardware-tweaks.nix
+++ modules/nixos/hardware-tweaks.nix
@@ -7,4 +7,10 @@
   boot.extraModprobeConfig = ''
     options 88x2bu rtw_switch_usb_mode=1 rtw_power_mgnt=0 rtw_ips_mode=0 rtw_enusbss=0 rtw_beamforming_enable=0 rtw_vht_enable=2 rtw_stbc_enable=0
   '';
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 50;
+  };
 }
